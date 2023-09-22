60373482e8a05c2876df41402427a2f6c6ed9b44
MVinesh4886 <vineshkrishna26@gmail.com>
2023-09-22 20:20:56 +0530

    created Post

--- app.js
+++ app.js
@@ -1,15 +1,42 @@
 const http = require("http");
 const fs = require("fs");
+
 const server = http.createServer(function (req, res) {
   //console.log("Krishna"); printing the name in the command prompt
   //console.log(req.url, req.method, req.headers);
   // creating a response
-  // res.setHeader("Content-Type", "text/html");
-  // res.write(`<html>
-  // <head><title>My First Page</title></head>
-  // <body><h1>Hello from Node.js Server!</h1></body>
-  // </html>`);
-  // res.end();
+  const url = req.url;
+
+  if (url === "/") {
+    res.write(`<html>
+    <head><title>My First Page</title></head>
+    <body><form action="/message">
+    <input type="text" name="message"><button type="submit">SEND</button></form></body>
+    </html>`);
+    return res.end();
+  }
+  if (url === "/message" && req.method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      console.log(parsedBody);
+      fs.writeFile("message.txt", parsedBody, (error) => {
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        res.end();
+      });
+    });
+  }
+  res.setHeader("Content-Type", "text/html");
+  res.write(`<html>
+  <head><title>My First Page</title></head>
+  <body><h1>Hello from Node.js Server!</h1></body>
+  </html>`);
+  res.end();
 });
 
 server.listen(4000, () => {
@@ -62,47 +89,47 @@ server.listen(4000, () => {
 // );
 
 //Listen to event
-server.on("request", (req, res) => {
-  console.log(req, req.method);
-  //get the url
-  const url = req.url;
-  if (url === "/") {
-    //read content on login html page
-    fs.readFile("home.html", (err, data) => {
-      //check err
-      if (err) {
-        console.log(err);
-      } else {
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.write(data);
-        res.end();
-      }
-    });
-  }
-  if (url === "/about") {
-    //read content on login html page
-    fs.readFile("about.html", (err, data) => {
-      //check err
-      if (err) {
-        console.log(err);
-      } else {
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.write(data);
-        res.end();
-      }
-    });
-  }
-  if (url === "/node") {
-    //read content on login html page
-    fs.readFile("node.html", (err, data) => {
-      //check err
-      if (err) {
-        console.log(err);
-      } else {
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.write(data);
-        res.end();
-      }
-    });
-  }
-});
+// server.on("request", (req, res) => {
+//   console.log(req, req.method);
+//   //get the url
+//   const url = req.url;
+//   if (url === "/") {
+//     //read content on login html page
+//     fs.readFile("home.html", (err, data) => {
+//       //check err
+//       if (err) {
+//         console.log(err);
+//       } else {
+//         res.writeHead(200, { "Content-Type": "text/html" });
+//         res.write(data);
+//         res.end();
+//       }
+//     });
+//   }
+//   if (url === "/about") {
+//     //read content on login html page
+//     fs.readFile("about.html", (err, data) => {
+//       //check err
+//       if (err) {
+//         console.log(err);
+//       } else {
+//         res.writeHead(200, { "Content-Type": "text/html" });
+//         res.write(data);
+//         res.end();
+//       }
+//     });
+//   }
+//   if (url === "/node") {
+//     //read content on login html page
+//     fs.readFile("node.html", (err, data) => {
+//       //check err
+//       if (err) {
+//         console.log(err);
+//       } else {
+//         res.writeHead(200, { "Content-Type": "text/html" });
+//         res.write(data);
+//         res.end();
+//       }
+//     });
+//   }
+// });
