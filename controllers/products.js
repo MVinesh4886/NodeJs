ead8e4eca33cafbae188e698f7d9a9c2047883df
MVinesh4886 <vineshkrishna26@gmail.com>
2023-09-29 19:52:31 +0530

    created models

--- controllers/products.js
+++ controllers/products.js
@@ -1,5 +1,6 @@
 const path = require("path");
-const products = [];
+// const products = [];
+const Product = require("../models/product");
 
 exports.getAddproduct = (req, res, next) => {
   res.sendFile(path.join(__dirname, "../", "views", "addproduct.html"));
@@ -7,10 +8,13 @@ exports.getAddproduct = (req, res, next) => {
 
 exports.postAddproduct = (req, res, next) => {
   console.log(req.body);
-  products.push(req.body);
-  //   res.redirect("/");
+  const product = new Product(req.body.title);
+  product.save();
+  //   products.push(req.body);
+  res.redirect("/");
 };
 
 exports.getProducts = (req, res, next) => {
+  const products = Product.fetchAll();
   res.sendFile(path.join(__dirname, "../", "views", "shop.html"));
 };
