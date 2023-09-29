3096b1221bd748b6f29841a7e022a38934823866
MVinesh4886 <vineshkrishna26@gmail.com>
2023-09-29 13:28:43 +0530

    created & updated views/path/public

--- routes/admin.js
+++ routes/admin.js
@@ -1,14 +1,42 @@
-//import express
+// create an admin route or addproduct route to the main server that is app.js
+// //import express
+// const express = require("express");
+
+// const router = express.Router();
+
+// //admin/addproduct -GET
+// router.get("/addproduct", (req, res, next) => {
+//   res.send(
+//     //at first, we have just "/addProduct" and now we change it to /admin/addProduct
+//     '<form action="/admin/addproduct" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
+//   );
+// });
+
+// //here, "/product" was used before. Now we changed it to "/addproduct"
+
+// //admin/addproduct -POST
+// router.post("/addproduct", (req, res, next) => {
+//   console.log(req.body);
+//   res.redirect("/");
+// });
+
+// module.exports = router;
+
+//serving addproduct file from views to the main server app.js
+const path = require("path");
 const express = require("express");
+
+//we import the path
+const rootDir = require("../util/path");
 
 const router = express.Router();
 
 //admin/addproduct -GET
 router.get("/addproduct", (req, res, next) => {
-  res.send(
-    //at first, we have just "/addProduct" and now we change it to /admin/addProduct
-    '<form action="/admin/addproduct" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
-  );
+  // res.sendFile(path.join(__dirname, "../", "views", "addproduct.html"));
+
+  //above code is step by step approach which we can follow otherwise this is much cleaner way to sendFile
+  res.sendFile(path.join(rootDir, "views", "addproduct.html"));
 });
 
 //here, "/product" was used before. Now we changed it to "/addproduct"
--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,26 @@
+// create an shop router and export to the main server that is app.js
+// const express = require("express");
+
+// const router = express.Router();
+
+// router.get("/", (req, res, next) => {
+//   res.send("<h1>Hello from Express!</h1>");
+// });
+
+// module.exports = router;
+
+//serving shop.html file to the main server that is app.js
+const path = require("path");
 const express = require("express");
+
+const rootDir = require("../util/path");
 
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  res.send("<h1>Hello from Express!</h1>");
+  // res.sendFile(path.join(__dirname, "../", "views", "shop.html"));
+
+  res.sendFile(path.join(rootDir, "views", "shop.html"));
 });
 
 module.exports = router;
